c502814f361ee9d568d340e9213920723c98ceff
Kelsey <kelsey.jewell@icloud.com>
2016-08-13 21:42:06 -0700

    first front end commit

--- app/src/main/java/rainbow_unicorns/rainbow_unicorns/components/home/HomeActivity.java
+++ app/src/main/java/rainbow_unicorns/rainbow_unicorns/components/home/HomeActivity.java
@@ -1,23 +1,64 @@
 package rainbow_unicorns.rainbow_unicorns.components.home;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import butterknife.OnClick;
 import rainbow_unicorns.rainbow_unicorns.R;
+import rainbow_unicorns.rainbow_unicorns.models.Restaurant;
 
 public class HomeActivity extends AppCompatActivity {
     private ImageView appImageView;
     private ImageButton imageButton;
+    private Queue<Restaurant> allRest;
+    private Restaurant currRest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void loadAllRestaurants() {
+        allRest = new LinkedList<Restaurant>();
+        allRest.add(new Restaurant("temp"));
+        allRest.add(new Restaurant("temp2"));
+        allRest.add(new Restaurant("temp3"));
+    }
+
+    private Restaurant setRestaurant() {
+        currRest = allRest.remove();
+        return currRest;
+    }
+
+
+    @OnClick(R.id.imageButtonNo)
+    private void onNoButtonClicked() {
+        //loadNextOption();
+    }
+
+    @OnClick(R.id.imageButtonYes)
+    private void onYesButtonClicked() {
+        setRestaurant();
+        //storeInDecisionList();
+        //displayFinalDecision();
 
     }
+
+    @OnClick(R.id.imageButtonMaybe)
+    private void onMaybeButtonClicked() {
+        setRestaurant();
+        //storeInDecisionList();
+        //loadNextOption();
+    }
+
+
+
 
 
 }
--- app/src/main/java/rainbow_unicorns/rainbow_unicorns/models/Restaurant.java
+++ app/src/main/java/rainbow_unicorns/rainbow_unicorns/models/Restaurant.java
@@ -1,4 +1,13 @@
 package rainbow_unicorns.rainbow_unicorns.models;
 
 public class Restaurant {
+    private String name;
+
+    public Restaurant(String temp) {
+        this.name = temp;
+    }
+
+    private String getName() {
+        return this.name;
+    }
 }

154fcd2d451a0599a68d01c5cd417ae50c5b75b7
David Zhu <hdavidzhu@gmail.com>
2016-08-13 17:00:06 -0700

    ADD: Retrofit and two other screens.

---
+++ app/src/main/java/rainbow_unicorns/rainbow_unicorns/components/decisions/DecisionsActivity.java
@@ -0,0 +1,15 @@
+package rainbow_unicorns.rainbow_unicorns.components.decisions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rainbow_unicorns.rainbow_unicorns.R;
+
+public class DecisionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_decisions);
+    }
+}
